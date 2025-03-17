c1853991159919c1bb8e7363be2b30eb4f4cb13c
Eoghan West <eowest@gmail.com>
2025-03-16 22:15:37 -0400

    working prototype

--- src/bin/bg-serv.rs
+++ src/bin/bg-serv.rs
@@ -1,35 +1,111 @@
-use std::f32::consts::PI;
-
+// use anyhow::Result;
 use bevy::{
+    app::{RunMode, ScheduleRunnerPlugin},
     log::{Level, LogPlugin},
     pbr::wireframe::{WireframeConfig, WireframePlugin},
     prelude::*,
-    window::{PresentMode, WindowLevel, WindowMode},
+    render::RenderPlugin,
+    // window::{PresentMode, WindowLevel, WindowMode},
+    winit::WinitPlugin,
 };
+use bevy_capture::{
+    CameraTargetHeadless, Capture, CaptureBundle, Encoder,
+    encoder::{
+        gif::{self, GifEncoder},
+        mp4_openh264::Mp4Openh264Encoder,
+    },
+};
+use rml_rtmp::time::RtmpTimestamp;
+use tokio::{net::TcpListener, sync::mpsc::UnboundedSender};
+// use rtmp::{
+//     // relay::{pull_client::PullClient, push_client::PushClient},
+//     rtmp::RtmpServer,
+// };
+use std::{
+    f32::consts::PI,
+    fs::File,
+    io::{Read, Seek, Stdout, Write},
+    time::Instant,
+};
+// use streamhub::{StreamsHub, define::StreamHubEventSender};
 
 /// A marker component for our shapes so we can query them separately from the ground plane
 #[derive(Component)]
 struct Shape;
 
-fn main() {
+#[derive(Resource, Clone)]
+struct Sender {
+    epoch: Instant,
+    sender: UnboundedSender<StreamManagerMessage>,
+}
+
+impl Write for Sender {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        if let Err(e) = self.sender.send(StreamManagerMessage::NewVideoData {
+            timestamp: RtmpTimestamp {
+                value: self.epoch.elapsed().as_millis() as u32,
+            },
+            data: buf.to_owned().into(),
+        }) {
+            println!("{e}");
+        }
+
+        Ok(buf.len())
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        Ok(())
+    }
+}
+
+impl Seek for Sender {
+    fn seek(&mut self, pos: std::io::SeekFrom) -> std::io::Result<u64> {
+        // Ok(1920 * 1080 * 4)
+        Ok(0)
+    }
+}
+
+impl Encoder for Sender {
+    fn encode(&mut self, image: &Image) -> bevy_capture::encoder::Result<()> {
+        // if let Err(e) = self.sender.send(StreamManagerMessage::NewVideoData {
+        //     timestamp: RtmpTimestamp {
+        //         value: self.epoch.elapsed().as_millis() as u32,
+        //     },
+        //     data: image.data.to_owned().into(),
+        // }) {
+        //     println!("{e}");
+        // }
+        let image = image.clone().try_into_dynamic()?;
+        let buffer = image.to_rgba8();
+
+        // buffer.to_owned().iter().for_each(|byte| print!("{byte}"));
+        buffer.save("/tmp/frame.png")?;
+
+        Ok(())
+    }
+}
+
+#[tokio::main]
+async fn main() {
     App::new()
         .add_plugins((
             DefaultPlugins
+                .set(ImagePlugin::default_nearest())
                 .set(LogPlugin {
                     level: Level::INFO,
                     ..default()
                 })
-                .set(WindowPlugin {
-                    primary_window: Some(Window {
-                        present_mode: PresentMode::AutoVsync,
-                        name: Some("game-bg".into()),
-                        window_level: WindowLevel::AlwaysOnBottom,
-                        mode: WindowMode::BorderlessFullscreen(MonitorSelection::Current),
-                        ..Default::default()
-                    }),
-                    ..Default::default()
+                .disable::<WinitPlugin>()
+                .set(RenderPlugin {
+                    synchronous_pipeline_compilation: true,
+                    ..default()
                 }),
             WireframePlugin,
+            // Add the ScheduleRunnerPlugin to run the app in loop mode
+            ScheduleRunnerPlugin {
+                run_mode: RunMode::Loop { wait: None },
+            },
+            bevy_capture::CapturePlugin,
         ))
         .insert_resource(WireframeConfig {
             // The global wireframe config enables drawing of wireframes on every mesh,
@@ -46,12 +122,13 @@ fn main() {
             }
             .into(),
         })
+        // .insert_resource(sender)
         .add_systems(Startup, (camera_setup, spawn_cube))
-        .add_systems(Update, rotate)
+        .add_systems(Update, (rotate, update))
         .run();
 }
 
-fn camera_setup(mut commands: Commands) {
+fn camera_setup(mut commands: Commands, mut images: ResMut<Assets<Image>>) {
     commands.insert_resource(ClearColor(
         Srgba {
             red: (30. / 255.),
@@ -65,9 +142,8 @@ fn camera_setup(mut commands: Commands) {
     commands.spawn((
         Camera3d::default(),
         Transform::from_xyz(0.0, 0.0, 8.0).looking_at(Vec3::new(0.0, 0.0, 0.0), Vec3::Y),
-        Camera::default(),
-        // VisualizationCamera,
-        // ClearColorConfig: (Color::BLACK),
+        Camera::default().target_headless(1920, 1080, &mut images),
+        CaptureBundle::default(),
     ));
 
     commands.spawn((
@@ -101,5 +177,21 @@ fn spawn_cube(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>) {
 fn rotate(mut query: Query<&mut Transform, With<Shape>>, time: Res<Time>) {
     for mut transform in &mut query {
         transform.rotate_y(time.delta_secs());
+    }
+}
+
+// Start capturing
+fn update(mut capture: Query<&mut Capture> /* , sender: ResMut<Sender> */) {
+    let mut capture = capture.single_mut();
+    if !capture.is_capturing() {
+        capture.start(
+            GifEncoder::new(File::create("/tmp/frame.gif").unwrap())
+                .with_repeat(gif::Repeat::Infinite),
+            // Mp4Openh264Encoder::new(sender.clone(), 1920, 1080).unwrap(),
+            // Mp4Openh264Encoder::new(File::create("/tmp/tmp-bevy.mp4").unwrap(), 1920, 1080)
+            //     .unwrap(),
+            // sender.clone(),
+        );
+        info!("started");
     }
 }
---
+++ src/lib.rs
@@ -0,0 +1,26 @@
+use std::fmt::Display;
+use tokio::sync::mpsc::UnboundedSender;
+
+// pub mod connection;
+// pub mod stream_manager;
+
+pub fn spawn<F, E>(future: F)
+where
+    F: Future<Output = Result<(), E>> + Send + 'static,
+    E: Display,
+{
+    tokio::task::spawn(async {
+        if let Err(error) = future.await {
+            eprintln!("{}", error);
+        }
+    });
+}
+
+/// Sends a message over an unbounded receiver and returns true if the message was sent
+/// or false if the channel has been closed.
+pub fn send<T>(sender: &UnboundedSender<T>, message: T) -> bool {
+    match sender.send(message) {
+        Ok(_) => true,
+        Err(_) => false,
+    }
+}
